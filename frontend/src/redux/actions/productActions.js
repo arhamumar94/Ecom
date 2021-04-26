292c3e2025e791ee0fba53f5c6a04cc614855233
arhamumar94 <arhamumar1999@gmail.com>
2021-04-26 18:31:47 +0530

    hello

--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -5,7 +5,7 @@ export const getProducts = () => async (dispatch) => {
   try {
     dispatch({ type: actionTypes.GET_PRODUCTS_REQUEST });
 
-    const { data } = await axios.get("https://ecoarham.herokuapp.com/api/products/https://storage4updater.blob.core.windows.net/public-data/export.json");
+    const { data } = await axios.get("https://ecoarham.herokuapp.com/api/products");
 
     dispatch({
       type: actionTypes.GET_PRODUCTS_SUCCESS,
